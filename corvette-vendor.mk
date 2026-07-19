ee29a176df20982f14684513358d326ad85d943b
chandu078 <chandudyavanapelli03@gmail.com>
2026-07-19 12:17:26 +0000

    corvette: De-blob vendor.oplus.hardware.displaycolorfeature V1 interface
    Change-Id: Idea861e46fb62b2c712048cb2eab3d47ad13eb6b

--- corvette-vendor.mk
+++ corvette-vendor.mk
@@ -1469,7 +1469,6 @@ PRODUCT_PACKAGES += \
     vendor.display.postproc@1.0 \
     vendor.oplus.hardware.camera.aon-service-impl \
     vendor.oplus.hardware.cammidasservice@1.0 \
-    vendor.oplus.hardware.displaycolorfeature-V1-ndk_vendor \
     vendor.pixelworks.hardware.display-V2-ndk_vendor \
     vendor.pixelworks.hardware.display@1.0_vendor \
     vendor.pixelworks.hardware.display@1.1_vendor \
@@ -1729,7 +1728,6 @@ PRODUCT_PACKAGES += \
     libaiboost.oplusmss \
     libaiboost_qnn_external_delegate.oplusmss \
     vendor.oplus.hardware.camera_rfi-V1-service-impl \
-    vendor.oplus.hardware.displaycolorfeature-V1-ndk \
     vendor.oplus.hardware.sendextcamcmd-V1-service-impl \
     vendor.pixelworks.hardware.display-V1-ndk \
     vendor.pixelworks.hardware.display-V2-ndk \
